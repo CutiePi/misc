d3f6e76aafa40cccd9e7ff9bbb66c5f0c31114cd
Jasmin Lapointe <jasmin.lapointe@hotmail.ca>
2022-06-21 09:51:54 -0400

    Update onboardingInstall.sh

--- sh/onboardingInstall.sh
+++ sh/onboardingInstall.sh
@@ -2,10 +2,10 @@
 
 # manual update apt dependencies
 # https://dev.mysql.com/downloads/repo/apt/
-apt install curl
+apt install curl -y
 curl https://repo.mysql.com//mysql-apt-config_0.8.22-1_all.deb --output ./mysqlApt.deb
 apt install ./mysqlApt.deb
-apt update
+apt update -y
 
 # update apt dependencies
 # use node 14 repo, default apt is v10
@@ -18,14 +18,14 @@ snap install slack
 #snap install webstorm
 
 # install via apt & apt-get
-apt-get install redis-server
-apt-get install git-all
-apt-get install wkhtmltopdf
-apt install imagemagick
-apt install mysql-server
-apt install mysql-client
-apt install mysql-workbench-community
-apt install nodejs
+apt-get install redis-server -y
+apt-get install git-all -y
+apt-get install wkhtmltopdf -y
+apt install imagemagick -y
+apt install mysql-server -y
+apt install mysql-client -y
+apt install mysql-workbench-community -y
+apt install nodejs -y
 
 # install via npm, after nodejs
 npm install --global yarn
@@ -39,5 +39,5 @@ echo 'notify-keyspace-events "AKE"' >> /etc/redis/redis.conf
 
 
 # configure stuff
-# TODO add what to pick
+# user root password root
  mysql_secure_installation
