4dc71c8c52a420dfaf37d3d51a697d8c07de207d
Jasmin Lapointe <jasmin.lapointe@hotmail.ca>
2022-06-23 16:46:17 -0400

    Update onboardingInstall.sh

--- sh/onboardingInstall.sh
+++ sh/onboardingInstall.sh
@@ -26,6 +26,7 @@ apt install mysql-server -y
 apt install mysql-client -y
 apt install mysql-workbench-community -y
 apt install nodejs -y
+apt install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 
 # install via npm, after nodejs
 npm install --global yarn
@@ -34,10 +35,20 @@ npm install --global yarn
 
 
 # configs
+export NODE_ENV=development
 echo 'export NODE_ENV=development' >> ~/.bashrc
 echo 'notify-keyspace-events "AKE"' >> /etc/redis/redis.conf
 
+# generating your ssh key for git
+ssh-keygen -t ed25519
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-add ~/.ssh/id_ed25519
+
 
 # configure stuff
-# user root password root
+# user root password root and all defaults, refresh privileges
  mysql_secure_installation
+ 
+ 
+ cat ~/.ssh/id_ed25519.pub
+ echo "copy paste this into your github ssh keys"
