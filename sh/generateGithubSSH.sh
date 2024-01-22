d7c7929ac432a6bfc398ee8eac510eadad4048c1
Jasmin Lapointe <jasmin.lapointe@hotmail.ca>
2024-01-22 16:32:35 -0500

    add gpg signature

--- sh/generateGithubSSH.sh
+++ sh/generateGithubSSH.sh
@@ -5,3 +5,9 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 ssh-add ~/.ssh/id_ed25519
 echo "copy paste this into your github ssh keys"
 cat ~/.ssh/id_ed25519.pub
+
+# ADD gpg signature for commits
+# Note: SSH signature verification is available in Git 2.34 or later.
+git config --global commit.gpgsign true
+git config --global gpg.format ssh
+git config --global user.signingkey /Users/jasmin/.ssh/id_ed25519.pub
