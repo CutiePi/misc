a63e2b2166c708969c0dc6888b7552e749045769
Jasmin Lapointe <jasmin.lapointe@hotmail.ca>
2024-01-25 10:55:13 -0500

    Update macosbaseinstall.sh

--- sh/macosbaseinstall.sh
+++ sh/macosbaseinstall.sh
@@ -1,1 +1,21 @@
+echo "install curl & homebrew, before running script"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# sometimes the C+ inclusion path is empty when it needs to point on homebrew stuff
+# echo 'export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/opt/homebrew/include' >> ~/.zprofile
 
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# basic stuff
+brew install git
+#brew install n
+
+
+# terminal conf & env vars (~/.zshrc or ~/.zprofile or ~/.bash_profile)
+echo "alias ll='ls -la'" >> ~/.zshrc
+
+# enables auto complete for commands (not needed built in oh-my-zsh
+# echo "autoload -Uz compinit && compinit" >> ~/.zprofile
+# echo 'set completion-ignore-case on' >> ~/.inputrc
+# echo 'set show-all-if-ambiguous on' >> ~/.inputrc
+# echo 'TAB: menu-complete' >> ~/.inputrc
